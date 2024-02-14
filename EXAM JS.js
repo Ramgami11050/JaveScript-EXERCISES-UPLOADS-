6b3dc4d7c0c2ea9024b71b8bbdf37e84eed52d5a
Ramgami11050 <154421131+Ramgami11050@users.noreply.github.com>
2024-02-14 13:15:41 +0100

    Add files via upload
    Exam Updates

--- EXAM JS.js
+++ EXAM JS.js
@@ -1,29 +1,86 @@
-function start(){
+function start() {
+    // TITLE
+    var title = document.createElement('h1');
+    title.innerText = "STUDENT DATA";
+    title.style.font = "italic bold 27px courier";
+    title.setAttribute("align", "center");
+    document.body.appendChild(title);
 
-// INPUT 1
-    var input = document.createElement("input");
-    input.type = "text";
-    input.className = "css-class-name"; // set the CSS class
-    document.body.appendChild(input); // put it into the DOM
+    // [] INPUT 1
+    var input = document.createElement("input"); // creates input ele
+    input.type = "text"; // sets input text
+    input.className = "Name1"; // set the CSS class
+    input.id = "Name1Test";
+    document.body.appendChild(input).style.marginTop = "7px"; // puts into body
+    
+    if (button.onclick) {
+    var check1 = document.getElementById("Name1Test").required = true;
+    document.body.getElementById("Name1Test").innerHTML = check1;
+    }
 
-// DIV 1
-var div = document.createElement("div"); //adds div space
-document.body.appendChild(div);
+    // DIV 1
+    var div = document.createElement("div"); //adds div space
+    document.body.appendChild(div);
 
 
-// INPUT 2
+    // [] INPUT 2
     var input2 = document.createElement("input");
     input2.type = "text";
-    input2.className = "css-class-name"; // set the CSS class
-    document.body.appendChild(input2); // put it into the DOM
+    input2.className = "Name2";
+    input2.id = "Name2Test";
+    document.body.appendChild(input2).style.marginTop = "7px";
 
-// INPUT 3
+    // DIV 2
+    var div2 = document.createElement("div"); //adds div space
+    document.body.appendChild(div2);
 
+    // [] INPUT 3
+    var input3 = document.createElement("input");
+    input3.type = "text";
+    input3.className = "Name3";
+    input3.id = "Name3Test";
+    document.body.appendChild(input3).style.marginTop = "7px";
+
+    // DIV 3
+    var div3 = document.createElement("div"); //adds div space
+    document.body.appendChild(div3);
+
+    // [] BUTTON (SUBMIT)
+    var button = document.createElement("button");
+    button.id = "button1";
+    document.body.appendChild(button).style.padding = "10px 80px 10px 80px";
+
+
+    // ———————————————————————————————————————————————————————————————————————
+
+
+    // NAME (REQUIRED AND NO NUMBERS)
+    button.onclick = function () {
+        // [NAME]
+    let selectall = document.getElementById("Name1Test").value; // Selects value inside form
+    console.log("(1) Student Name Saved") // Displays value inside of fist form
+        // [AGE]
+    let selectall2 = document.getElementById("Name2Test").value; // Selects value inside form
+    console.log("(2) Student Age Saved") // Displays value inside of fist form
+        // [SUBJECT]
+    let selectall3 = document.getElementById("Name3Test").value; // Selects value inside form
+    console.log("(3) Student Subject Saved") // Displays value inside of fist form
+
+            alert("Information Saved");
+
+            const forminfo = {
+                Name: selectall,
+                Age: selectall2,
+                Subject: selectall3,
+            };
+
+            // Obtenemos las claves en un array
+            const keys = Object.keys(forminfo);
+            console.log(keys)
+
+    }
 }
-start()
-
-
-
+    start()
 
 
 
